3b7549ede29640cf2f3c8f898ecf911c3ecdcfe0
Saisri Gandla <gandlasaisri895@gmail.com>
2026-04-10 03:29:50 +0530

    Security Refactor
    Refactor Server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-const dbURI = "mongodb+srv://gandlasaisri895_db_user:1913219@smartcampusbuddy.6bqvutk.mongodb.net/?appName=SmartCampusBuddy";
+const dbURI = process.env.MONGO_URI;
 mongoose.connect(dbURI)
     .then(() => console.log('✅ Connected to MongoDB Atlas'))
     .catch(err => console.error('❌ MongoDB Connection Error:', err.message));
